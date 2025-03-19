c67bc2afda3a1f08fd37c23281d115da0c03e798
AlexButnariu31 <a.butnariu81@gmail.com>
2025-03-19 07:07:01 +0200

    Update Persoana.cs

--- Proiect_piu/Client/Persoana.cs
+++ Proiect_piu/Client/Persoana.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
@@ -10,6 +10,8 @@ namespace incercare_tema
 {
     public class Persoana
     {
+        private const char SEPARATOR_FISIER = ';';
+
         string nume;
         string prenume;
 
@@ -29,5 +31,50 @@ namespace incercare_tema
         {
             return $"Nume: {nume}, Prenume: {prenume}";
         }
+
+        public string Conv_sir_fisier()
+        {
+            string obiect_pers_fis = string.Format("{1}{0}{2}{0}",
+                SEPARATOR_FISIER,
+                (nume ?? "NECUNOSCUT"),
+                (prenume ?? "NECUNOSCUT")
+                );
+            return obiect_pers_fis;
+        }
+
+        public Persoana(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_FISIER);
+            this.nume = dateFisier[0];
+            this.prenume = dateFisier[1];
+        }
+
+        /*
+         * public string ConversieLaSir_PentruFisier()
+    {
+        string obiectStudentPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}",
+            SEPARATOR_PRINCIPAL_FISIER,
+            IdStudent.ToString(),
+            (Nume ?? " NECUNOSCUT "),
+            (Prenume ?? " NECUNOSCUT "),
+            (string.Join("|", note)));
+
+        return obiectStudentPentruFisier;
+    }
+
+
+    public Student(string linieFisier)
+    {
+        var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+        //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+        this.IdStudent = Convert.ToInt32(dateFisier[ID]);
+        this.Nume = dateFisier[NUME];
+        this.Prenume = dateFisier[PRENUME];
+        string[] vNote = dateFisier[3].Split('|');
+        note = new int[vNote.Length];
+        SetNote(vNote);
+    }
+         */
     }
 }
