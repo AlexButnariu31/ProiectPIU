4f1162538b3ad206240673db1522a97638827a3e
AlexButnariu31 <a.butnariu81@gmail.com>
2025-03-19 07:05:57 +0200

    Update Calculator.cs

--- Proiect_piu/produs/Calculator.cs
+++ Proiect_piu/produs/Calculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,8 @@ namespace incercare_tema
 {
     public class Calculator
     {
+        private const char SEPARATOR_FISIER = ';';
+
         string nume, marca ;
 
         public Calculator()
@@ -24,6 +26,23 @@ namespace incercare_tema
         public string Info()
         {
             return $"Nume:{nume}, Marca:{marca}";
+        }
+
+         public string Conv_sir_fisier()
+        {
+            string obiect_pers_fis = string.Format("{1}{0}{2}{0}",
+                SEPARATOR_FISIER,
+                (nume ?? "NECUNOSCUT"),
+                (marca ?? "NECUNOSCUT")
+                );
+            return obiect_pers_fis;
+        }
+
+        public Calculator(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_FISIER);
+            this.nume = dateFisier[0];
+            this.marca = dateFisier[1];
         }
     }
 }
