88c92faa98e48e32782a66797a189e701d24087b
AlexButnariu31 <a.butnariu81@gmail.com>
2025-03-25 20:31:10 +0200

    Add files via upload

---
+++ Proiect_piu/produs/Servicii.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace produs
+{
+    public enum Servicii
+    {
+        Reparare= 0,
+        Verificare = 1,
+        Schimbare_piese = 2,
+        Clonare_Hardrive = 4,
+        Instalare_os = 8
+    };
+}
