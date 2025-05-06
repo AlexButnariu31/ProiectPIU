9da7b9c7df1461541337550240ee90df0e81e3ec
AlexButnariu31 <a.butnariu81@gmail.com>
2025-05-06 20:41:02 +0300

    Imbunatatire Calculator.cs

--- Proiect_piu/produs/Calculator.cs
+++ Proiect_piu/produs/Calculator.cs
@@ -2,42 +2,51 @@ using produs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace incercare_tema
 {
     public class Calculator
     {
         private const char SEPARATOR_FISIER = ';';
-        public Servicii serv { get; set; }
-        string nume { get; set; }
-        string marca { get; set; }
+        private const char SEPARATOR_SERVICII = ',';
+        private const int MAX_SERVICII = 3;
+
+        public Servicii[] serv { get; private set; }
+        public string nume { get; set; }
+        public string marca { get; set; }
+        public string tip { get; set; }
 
         public Calculator()
         {
             nume = string.Empty;
             marca = string.Empty;
+            tip = string.Empty;
+            serv = Array.Empty<Servicii>();
         }
 
-        public Calculator(string nume, string marca)
+        public Calculator(string nume, string marca, string tip, Servicii[] servicii)
         {
             this.nume = nume;
             this.marca = marca;
-        }
-        public string Info()
-        {
-            return $"Nume:{nume}, Marca:{marca}, Scopul aducerii: {serv}";
+            this.tip = tip;
+            SetServicii(servicii);
         }
 
-         public string Conv_sir_fisier()
+        public string Info()
         {
-            string obiect_pers_fis = string.Format("{1}{0}{2}{0}{3}",
+            string serviciiStr = serv.Length > 0 ? string.Join(", ", serv.Select(s => s.ToString())) : "Niciun serviciu";
+            return $"Nume:{nume}, Marca:{marca}, Tip:{tip}, Servicii: {serviciiStr}";
+        }
+
+        public string Conv_sir_fisier()
+        {
+            string serviciiStr = serv.Length > 0 ? string.Join(SEPARATOR_SERVICII.ToString(), serv.Select(s => (int)s)) : "";
+            return string.Format("{1}{0}{2}{0}{3}{0}{4}",
                 SEPARATOR_FISIER,
                 (nume ?? "NECUNOSCUT"),
                 (marca ?? "NECUNOSCUT"),
-                serv);
-            return obiect_pers_fis;
+                (tip ?? "NECUNOSCUT"),
+                serviciiStr);
         }
 
         public Calculator(string linieFisier)
@@ -45,7 +54,36 @@ namespace incercare_tema
             var dateFisier = linieFisier.Split(SEPARATOR_FISIER);
             this.nume = dateFisier[0];
             this.marca = dateFisier[1];
-            serv = (Servicii)Enum.Parse(typeof(Servicii), dateFisier[2]);
+            this.tip = dateFisier.Length > 2 ? dateFisier[2] : string.Empty;
+
+            if (dateFisier.Length > 3 && !string.IsNullOrEmpty(dateFisier[3]))
+            {
+                var serviciiIds = dateFisier[3].Split(SEPARATOR_SERVICII)
+                                              .Where(s => !string.IsNullOrEmpty(s))
+                                              .Select(int.Parse)
+                                              .ToArray();
+                this.serv = serviciiIds.Select(id => (Servicii)id).ToArray();
+            }
+            else
+            {
+                this.serv = Array.Empty<Servicii>();
+            }
+        }
+
+        public void SetServicii(Servicii[] servicii)
+        {
+            if (servicii == null || servicii.Length == 0)
+            {
+                serv = Array.Empty<Servicii>();
+                return;
+            }
+
+            serv = servicii.Take(MAX_SERVICII).ToArray();
+        }
+
+        public Servicii[] GetServicii()
+        {
+            return serv;
         }
     }
 }
