75bbefbd36278064af097f874ab997f3f175378a
AlexButnariu31 <a.butnariu81@gmail.com>
2025-05-06 20:43:21 +0300

    Update Form2.cs

--- Proiect_piu/Formular/Form2.cs
+++ Proiect_piu/Formular/Form2.cs
@@ -8,32 +8,41 @@ using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Formular
 {
-    public partial class Form2: Form
+    public partial class Form2 : Form
     {
         private TextBox txtNume;
         private TextBox txtMarca;
-        private ComboBox cmbServiciu;
         private ListBox listBoxCalculatoare;
         private Administrare_produs_fisier adminCalc;
+        private CheckBox[] serviceCheckBoxes;
+        private RadioButton[] typeRadioButtons;
+
         public Form2()
         {
             string numeFisier = ConfigurationManager.AppSettings["NumeFisier_pc"];
             adminCalc = new Administrare_produs_fisier(numeFisier);
 
+            InitializeForm();
+            InitializeControls();
+        }
+
+        private void InitializeForm()
+        {
             this.Size = new Size(1000, 650);
             this.StartPosition = FormStartPosition.CenterScreen;
             this.Font = new Font("Comic Sans MS", 12, FontStyle.Regular);
-            this.ForeColor = Color.Black; // Text negru
+            this.ForeColor = Color.Black;
             this.Text = "Gestionare Calculatoare Service";
             this.BackColor = Color.White;
             this.MinimumSize = new Size(800, 500);
+        }
 
+        private void InitializeControls()
+        {
             SplitContainer mainSplitContainer = new SplitContainer
             {
                 Dock = DockStyle.Fill,
@@ -41,6 +50,7 @@ namespace Formular
                 SplitterDistance = 70,
                 FixedPanel = FixedPanel.Panel1
             };
+
             Panel headerPanel = new Panel
             {
                 Dock = DockStyle.Fill,
@@ -58,13 +68,15 @@ namespace Formular
                 Height = 50
             };
             headerPanel.Controls.Add(lblHeader);
+
             SplitContainer contentSplit = new SplitContainer
             {
                 Dock = DockStyle.Fill,
                 Orientation = Orientation.Vertical,
-                SplitterDistance = 250,
+                SplitterDistance = 350,
                 FixedPanel = FixedPanel.None
             };
+
             Panel leftPanel = new Panel
             {
                 Dock = DockStyle.Fill,
@@ -72,12 +84,29 @@ namespace Formular
                 BackColor = Color.WhiteSmoke,
                 AutoScroll = true
             };
+
             Panel rightPanel = new Panel
             {
                 Dock = DockStyle.Fill,
                 Padding = new Padding(30),
                 AutoScroll = true
             };
+
+            AddComputerControls(leftPanel);
+
+            AddDisplayControls(rightPanel);
+
+            mainSplitContainer.Panel1.Controls.Add(headerPanel);
+            contentSplit.Panel1.Controls.Add(leftPanel);
+            contentSplit.Panel2.Controls.Add(rightPanel);
+            mainSplitContainer.Panel2.Controls.Add(contentSplit);
+
+            this.Controls.Add(mainSplitContainer);
+            this.Resize += (sender, e) => AdjustListBoxSize(rightPanel);
+        }
+
+        private void AddComputerControls(Panel panel)
+        {
             Label lblAddComputer = new Label
             {
                 Text = "Adaugă Calculator Nou",
@@ -88,7 +117,8 @@ namespace Formular
                 Width = 200,
                 Height = 40
             };
-            leftPanel.Controls.Add(lblAddComputer);
+            panel.Controls.Add(lblAddComputer);
+
             Label lblNume = new Label
             {
                 Text = "Nume calculator:",
@@ -96,7 +126,7 @@ namespace Formular
                 Left = 20,
                 Width = 200
             };
-            leftPanel.Controls.Add(lblNume);
+            panel.Controls.Add(lblNume);
 
             txtNume = new TextBox
             {
@@ -105,7 +135,8 @@ namespace Formular
                 Width = 200,
                 Height = 30
             };
-            leftPanel.Controls.Add(txtNume);
+            panel.Controls.Add(txtNume);
+
             Label lblMarca = new Label
             {
                 Text = "Marca:",
@@ -113,7 +144,7 @@ namespace Formular
                 Left = 20,
                 Width = 200
             };
-            leftPanel.Controls.Add(lblMarca);
+            panel.Controls.Add(lblMarca);
 
             txtMarca = new TextBox
             {
@@ -122,29 +153,67 @@ namespace Formular
                 Width = 200,
                 Height = 30
             };
-            leftPanel.Controls.Add(txtMarca);
-            Label lblServiciu = new Label
+            panel.Controls.Add(txtMarca);
+
+            Label lblTip = new Label
             {
-                Text = "Serviciu:",
+                Text = "Tip calculator:",
                 Top = txtMarca.Bottom + 20,
                 Left = 20,
                 Width = 200
             };
-            leftPanel.Controls.Add(lblServiciu);
+            panel.Controls.Add(lblTip);
 
-            cmbServiciu = new ComboBox
+            typeRadioButtons = new RadioButton[3];
+            string[] types = { "Gaming", "Office", "Home" };
+            int topPosition = lblTip.Bottom + 5;
+
+            for (int i = 0; i < types.Length; i++)
             {
-                Top = lblServiciu.Bottom + 5,
+                typeRadioButtons[i] = new RadioButton
+                {
+                    Text = types[i],
+                    Top = topPosition,
+                    Left = 20,
+                    Width = 100,
+                    Tag = types[i].ToLower()
+                };
+                panel.Controls.Add(typeRadioButtons[i]);
+                topPosition += 30;
+            }
+            typeRadioButtons[0].Checked = true;
+
+            Label lblServicii = new Label
+            {
+                Text = "Servicii:",
+                Top = topPosition + 10,
                 Left = 20,
-                Width = 200,
-                DropDownStyle = ComboBoxStyle.DropDownList
+                Width = 200
             };
-            cmbServiciu.Items.AddRange(Enum.GetNames(typeof(Servicii)));
-            leftPanel.Controls.Add(cmbServiciu);
+            panel.Controls.Add(lblServicii);
+
+            serviceCheckBoxes = new CheckBox[Enum.GetValues(typeof(Servicii)).Length];
+            topPosition = lblServicii.Bottom + 5;
+
+            foreach (Servicii service in Enum.GetValues(typeof(Servicii)))
+            {
+                int index = (int)service - 1;
+                serviceCheckBoxes[index] = new CheckBox
+                {
+                    Text = service.ToString().Replace('_', ' '),
+                    Top = topPosition,
+                    Left = 20,
+                    Width = 200,
+                    Tag = service
+                };
+                panel.Controls.Add(serviceCheckBoxes[index]);
+                topPosition += 30;
+            }
+
             Button btnAdauga = new Button
             {
                 Text = "Adaugă Calculator",
-                Top = cmbServiciu.Bottom + 30,
+                Top = topPosition + 20,
                 Left = 20,
                 Width = 200,
                 Height = 40,
@@ -152,28 +221,8 @@ namespace Formular
                 FlatStyle = FlatStyle.Flat,
                 Font = new Font("Comic Sans MS", 12, FontStyle.Bold)
             };
-            btnAdauga.Click += (sender, e) =>
-            {
-                if (!string.IsNullOrEmpty(txtNume.Text) && !string.IsNullOrEmpty(txtMarca.Text) && cmbServiciu.SelectedItem != null)
-                {
-                    Calculator newComputer = new Calculator(
-                        txtNume.Text,
-                        txtMarca.Text)
-                    {
-                        serv = (Servicii)Enum.Parse(typeof(Servicii), cmbServiciu.SelectedItem.ToString())
-                    };
-                    adminCalc.AddProdus(newComputer);
-                    MessageBox.Show("Calculator adăugat cu succes!", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    txtNume.Clear();
-                    txtMarca.Clear();
-                    cmbServiciu.SelectedIndex = -1;
-                }
-                else
-                {
-                    MessageBox.Show("Completați toate câmpurile!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            };
-            leftPanel.Controls.Add(btnAdauga);
+            btnAdauga.Click += BtnAdauga_Click;
+            panel.Controls.Add(btnAdauga);
 
             Button btnCauta = new Button
             {
@@ -187,22 +236,25 @@ namespace Formular
                 Font = new Font("Comic Sans MS", 12, FontStyle.Bold)
             };
             btnCauta.Click += BtnCauta_Click;
-            leftPanel.Controls.Add(btnCauta);
+            panel.Controls.Add(btnCauta);
 
             Button btnInapoi = new Button
             {
                 Text = "Înapoi",
                 Top = btnCauta.Bottom + 20,
-                Left = btnAdauga.Left,       
-                Width = btnAdauga.Width,     
-                Height = btnAdauga.Height,   
-                BackColor = btnAdauga.BackColor, 
+                Left = btnAdauga.Left,
+                Width = btnAdauga.Width,
+                Height = btnAdauga.Height,
+                BackColor = btnAdauga.BackColor,
                 FlatStyle = btnAdauga.FlatStyle,
-                Font = btnAdauga.Font        
+                Font = btnAdauga.Font
             };
             btnInapoi.Click += (sender, e) => this.Close();
-            leftPanel.Controls.Add(btnInapoi);
+            panel.Controls.Add(btnInapoi);
+        }
 
+        private void AddDisplayControls(Panel panel)
+        {
             Button btnAfiseaza = new Button
             {
                 Text = "Afișează Calculatoare",
@@ -214,83 +266,124 @@ namespace Formular
                 FlatStyle = FlatStyle.Flat,
                 Font = new Font("Comic Sans MS", 12, FontStyle.Bold)
             };
-            rightPanel.Controls.Add(btnAfiseaza);
+            panel.Controls.Add(btnAfiseaza);
+
             listBoxCalculatoare = new ListBox
             {
                 Top = btnAfiseaza.Bottom + 20,
                 Left = 20,
-                Width = rightPanel.Width + 480,
-                Height = rightPanel.Height - btnAfiseaza.Bottom - 50,
+                Width = panel.Width + 280,
+                Height = panel.Height - btnAfiseaza.Bottom - 50,
                 Font = new Font("Comic Sans MS", 12),
                 ForeColor = Color.Black,
                 BorderStyle = BorderStyle.FixedSingle,
                 BackColor = Color.White,
                 IntegralHeight = false
             };
-            rightPanel.Controls.Add(listBoxCalculatoare);
+            panel.Controls.Add(listBoxCalculatoare);
 
-            btnAfiseaza.Click += (sender, e) =>
-            {
-                int computerCount;
-                Calculator[] computerList = adminCalc.Get_produse(out computerCount);
-
-                listBoxCalculatoare.Items.Clear();
-                if (computerList != null && computerCount > 0)
-                {
-                    listBoxCalculatoare.BeginUpdate();
-                    foreach (Calculator calculator in computerList)
-                    {
-                        if (calculator != null)
-                        {
-                            listBoxCalculatoare.Items.Add(calculator.Info());
-                        }
-                    }
-                    listBoxCalculatoare.EndUpdate();
-
-                    int requiredHeight = (listBoxCalculatoare.Items.Count * listBoxCalculatoare.Font.Height) + 10;
-                    int maxHeight = rightPanel.Height - btnAfiseaza.Bottom - 50;
-                    listBoxCalculatoare.Height = Math.Min(requiredHeight, maxHeight);
-                }
-                else
-                {
-                    listBoxCalculatoare.Items.Add("Nu există calculatoare în fișier!");
-                    listBoxCalculatoare.ForeColor = Color.Red;
-                }
-            };
-
-            mainSplitContainer.Panel1.Controls.Add(headerPanel);
-            contentSplit.Panel1.Controls.Add(leftPanel);
-            contentSplit.Panel2.Controls.Add(rightPanel);
-            mainSplitContainer.Panel2.Controls.Add(contentSplit);
-
-            this.Controls.Add(mainSplitContainer);
-            this.Resize += (sender, e) =>
-            {
-                listBoxCalculatoare.Width = rightPanel.Width - 60;
-                int maxHeight = rightPanel.Height - btnAfiseaza.Bottom - 50;
-                listBoxCalculatoare.Height = maxHeight;
-            };
+            btnAfiseaza.Click += (sender, e) => DisplayComputers();
         }
+
+        private void AdjustListBoxSize(Panel panel)
+        {
+            listBoxCalculatoare.Width = panel.Width + 280;
+            int maxHeight = panel.Height - listBoxCalculatoare.Top - 20;
+            listBoxCalculatoare.Height = maxHeight;
+        }
+
+        private void BtnAdauga_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNume.Text) && !string.IsNullOrEmpty(txtMarca.Text))
+            {
+                string selectedType = typeRadioButtons.FirstOrDefault(r => r.Checked)?.Tag.ToString();
+
+                List<Servicii> selectedServices = new List<Servicii>();
+                foreach (var checkBox in serviceCheckBoxes)
+                {
+                    if (checkBox.Checked)
+                    {
+                        selectedServices.Add((Servicii)checkBox.Tag);
+                    }
+                }
+
+                if (selectedServices.Count == 0)
+                {
+                    MessageBox.Show("Selectați cel puțin un serviciu!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                Calculator newComputer = new Calculator(
+                    txtNume.Text,
+                    txtMarca.Text,
+                    selectedType,
+                    selectedServices.ToArray());
+
+                adminCalc.AddProdus(newComputer);
+                MessageBox.Show("Calculator adăugat cu succes!", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearInputFields();
+            }
+            else
+            {
+                MessageBox.Show("Completați toate câmpurile!", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ClearInputFields()
+        {
+            txtNume.Clear();
+            txtMarca.Clear();
+            typeRadioButtons[0].Checked = true;
+            foreach (var checkBox in serviceCheckBoxes)
+            {
+                checkBox.Checked = false;
+            }
+        }
+
+        private void DisplayComputers()
+        {
+            int computerCount;
+            Calculator[] computerList = adminCalc.Get_produse(out computerCount);
+
+            listBoxCalculatoare.Items.Clear();
+            if (computerList != null && computerCount > 0)
+            {
+                listBoxCalculatoare.BeginUpdate();
+                foreach (Calculator calculator in computerList)
+                {
+                    if (calculator != null)
+                    {
+                        listBoxCalculatoare.Items.Add(calculator.Info());
+                    }
+                }
+                listBoxCalculatoare.EndUpdate();
+            }
+            else
+            {
+                listBoxCalculatoare.Items.Add("Nu există calculatoare în fișier!");
+                listBoxCalculatoare.ForeColor = Color.Red;
+            }
+        }
+
         private void BtnCauta_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(txtNume.Text) ||!string.IsNullOrEmpty(txtMarca.Text) || cmbServiciu.SelectedItem != null)
+            string numeCautat = !string.IsNullOrEmpty(txtNume.Text) ? txtNume.Text.Trim() : null;
+            string marcaCautata = !string.IsNullOrEmpty(txtMarca.Text) ? txtMarca.Text.Trim() : null;
+            string tipCautat = typeRadioButtons.FirstOrDefault(r => r.Checked)?.Tag.ToString();
+
+            List<Servicii> selectedServices = new List<Servicii>();
+            foreach (var checkBox in serviceCheckBoxes)
             {
-                string numeCautat = !string.IsNullOrEmpty(txtNume.Text) ? txtNume.Text.Trim() : null;
-                string marcaCautata = !string.IsNullOrEmpty(txtMarca.Text) ? txtMarca.Text.Trim() : null;
-                Servicii? serviciuCautat = cmbServiciu.SelectedItem != null
-                    ? (Servicii)Enum.Parse(typeof(Servicii), cmbServiciu.SelectedItem.ToString())
-                    : (Servicii?)null;
-                Administrare_produs adminProd = new Administrare_produs();
-                int nrProduse;
-                Calculator[] produse = adminCalc.Get_produse(out nrProduse);
-
-                foreach (var prod in produse)
+                if (checkBox.Checked)
                 {
-                    if (prod != null)
-                        adminProd.AddProd(prod);
+                    selectedServices.Add((Servicii)checkBox.Tag);
                 }
+            }
+            Servicii[] serviciiCautate = selectedServices.ToArray();
 
-                List<Calculator> rezultate = adminProd.Cautare_prod(numeCautat, marcaCautata, serviciuCautat);
+            if (!string.IsNullOrEmpty(numeCautat) || !string.IsNullOrEmpty(marcaCautata) || serviciiCautate.Length > 0)
+            {
+                List<Calculator> rezultate = adminCalc.Cautare_prod(numeCautat, marcaCautata, tipCautat, serviciiCautate);
                 listBoxCalculatoare.Items.Clear();
                 if (rezultate.Count > 0)
                 {
