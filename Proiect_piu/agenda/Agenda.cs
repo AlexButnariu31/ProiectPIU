3cebf16a415ac7dab1e17c766b71c329f814d5cf
AlexButnariu31 <a.butnariu81@gmail.com>
2025-05-06 20:41:38 +0300

    Update Agenda.cs

--- Proiect_piu/agenda/Agenda.cs
+++ Proiect_piu/agenda/Agenda.cs
@@ -1,17 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using produs;
 
 namespace incercare_tema
 {
-    public class Agenda:Calculator
+    public class Agenda : Calculator
     {
         string cod;
         int pret;
         DateTime data_r;
+
         public Agenda()
         {
             cod = string.Empty;
@@ -19,16 +21,18 @@ namespace incercare_tema
             data_r = DateTime.MinValue;
         }
 
-        public Agenda( string nume, string marca, string _cod, int _pret):base(nume, marca)
+        public Agenda(string nume, string marca, string tip, Servicii[] servicii, string _cod, int _pret)
+            : base(nume, marca, tip, servicii)
         {
             cod = _cod;
             pret = _pret;
             data_r = DateTime.Now;
         }
+
         public string Info_a()
         {
             string s = Info();
-            return $"{s} Cod:{cod}, Data preluare:{data_r}, Pret:{pret}";
+            return $"{s} Cod:{cod}, Data preluare:{data_r.ToShortDateString()}, Pret:{pret}";
         }
     }
 }
